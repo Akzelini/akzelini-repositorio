c3cc48f7ce1e569dd73bbea58fc227463bbc4b42
Akzelini <distopicorizaceo@gmail.com>
2025-10-25 18:22:33 -0700

    Add files via upload

---
+++ musica.py
@@ -0,0 +1,6 @@
+playlist_musica = ["bohemian rhapsody", "hotel california",  "stairway to heaven",]
+playlist_musica.append("el hijo mayor")
+playlist_musica[1] = ("shape of you")
+playlist_musica[0] = ("watermelon sugar")
+playlist_musica.pop()
+print (playlist_musica)
